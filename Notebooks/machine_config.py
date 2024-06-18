fb6b9ad117a285ab329f0e4d5a0500e0392f44f4
Xuelong An Wang <awxlong@eduroam-int-dhcp-97-6-155.ucl.ac.uk>
2024-06-18 17:29:16 +0100

    fix syntax

--- Notebooks/machine_config.py
+++ Notebooks/machine_config.py
@@ -19,7 +19,7 @@ from dotenv import load_dotenv
 
 def setup_config(args):
         localcohort_name = args.cohort_name # e.g. "BRCA" for breast cancer
-        data_dir = f'{args.data_dir}{localcohort_name}/' # e.g. /SAN/ugi/WSI_Trans/BRCA/
+        data_dir = f"{args.data_dir}{localcohort_name}/" # e.g. /SAN/ugi/WSI_Trans/BRCA/
 
         
         #--------------------------> init machine and person
@@ -33,7 +33,7 @@ def setup_config(args):
                                         },
                               is_build = True)
         
-        exp_dir = f'{args.exp_dir}'                    # e.g. /home/xuelonan/g0_arrest/
+        exp_dir = f"{args.exp_dir}"                    # e.g. /home/xuelonan/g0_arrest/
         exp_locs = Locations(root=exp_dir,
                              sub_dirs={
                                         "src":"HistoMIL/",
@@ -47,18 +47,18 @@ def setup_config(args):
 
 
         # pdb.set_trace()
-        load_dotenv(dotenv_path=f'{args.api_dir}API.env')
+        load_dotenv(dotenv_path=f"{args.api_dir}API.env")
         
         machine = Machine(data_locs, exp_locs)
         user = Person(id=args.id)
         user.name = args.username
-        user.wandb_api_key = os.getenv('WANDB_API_KEY')
-        # hf_api_key = os.getenv('HF_READ_KEY')
+        user.wandb_api_key = os.getenv("WANDB_API_KEY")
+        # hf_api_key = os.getenv("HF_READ_KEY")
         # login(token=hf_api_key) # no need to initalize this now 
         # pdb.set_trace()
         # save as pickle
         loc = exp_locs.abs_loc("user")
-        with open(f"/{loc}/{localcohort_name}_machine_config.pkl", 'wb') as f:
+        with open(f"/{loc}/{localcohort_name}_machine_config.pkl", "wb") as f:
                 pickle.dump([data_locs, exp_locs, machine, user], f)
 
         print(f"User configuration settings stored in {loc}{localcohort_name}_machine_config.pkl!")
