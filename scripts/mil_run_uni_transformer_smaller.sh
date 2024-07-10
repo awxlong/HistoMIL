7fb7c4bffd0c8e43dc447a2b7743e32a874cf568
Xuelong An Wang <awxlong@eduroam-int-dhcp-97-6-155.ucl.ac.uk>
2024-07-10 17:20:31 +0100

    128

--- scripts/mil_run_uni_transformer_smaller.sh
+++ scripts/mil_run_uni_transformer_smaller.sh
@@ -1,15 +1,15 @@
-#$ -l tmem=8G
+#$ -l tmem=6G
 #$ -l h_rt=5:30:00 
 #$ -S /bin/bash
 #$ -j y
 #$ -l gpu=true,gpu_type=(a40|a100)
-#$ -N mil_transformer_uni_8epochs_smaller
+#$ -N mil_transformer_uni_8epochs_smaller_128
 echo "Running on host: $(hostname)"
 echo "Starting at: $(date)"
 cd secrier_lab/persistence/
 source /home/xuelonan/secrier_lab/python3.8.5-biomedai.source
 source /share/apps/source_files/cuda/cuda-11.8.source
-python3 HistoMIL/Notebooks/mil_run.py --exp-name 'mil_transformer_uni_8epochs_smaller' \
+python3 HistoMIL/Notebooks/mil_run.py --exp-name 'mil_transformer_uni_8epochs_smaller_128' \
                                       --project-name 'g0-arrest-uni' --wandb-entity-name 'cell-x' --localcohort-name 'COAD' --task-name 'g0_arrest' --pid-name 'PatientID' \
                                       --targets-name 'g0_arrest' \
                                       --cohort-dir '/home/xuelonan/secrier_lab/persistence/' \
